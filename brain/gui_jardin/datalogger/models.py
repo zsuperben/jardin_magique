402fb7bdb4e30ee87f4b1c0e20f0f32423237954
Benjamin Godard <ben@hacktopie.net>
2014-08-07 14:21:13 +0200

    first form for moist mesures

--- brain/gui_jardin/datalogger/models.py
+++ brain/gui_jardin/datalogger/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class SoilMoistMesure(models.Model):
    plant = models.CharField(max_length=100)
-   date  = models.DateTimeField('Date mesured')
+   time  = models.DateTimeField('Date mesured')
    value = models.IntegerField()
+   sec   = models.IntegerField()
+
+
+   
--- brain/gui_jardin/datalogger/views.py
+++ brain/gui_jardin/datalogger/views.py
@@ -8,5 +8,8 @@ from django import forms
 # Create your views here.
 def record_mesure(request):
     if request.method == 'POST' && "plant", "time", "value", "sec" in request.POST:
-        if request.POST
+        if request.POST["time"] * request.POST["value"] % 13 == request.POST["sec"]:
+            # Process data and save it to db. 
+            SoilMoistMesure.create(request.POST) # il faut ajouter le traitement des erreurs 
+
         
