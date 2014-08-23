40461cf6377211b0dc04914e9b6427a16ffa1df2
Benjamin Godard <ben@hacktopie.net>
2014-08-23 12:48:19 +0200

    starting to expand models.py in datalogger: plant types and more mesures

--- brain/gui_jardin/datalogger/models.py
+++ brain/gui_jardin/datalogger/models.py
@@ -2,10 +2,63 @@ from django.db import models
 
 # Create your models here.
 class SoilMoistMesure(models.Model):
-   plant = models.CharField(max_length=100)
-   time  = models.DateTimeField('Date mesured')
-   value = models.IntegerField()
-   sec   = models.IntegerField()
+   plant      = models.ForeignKey('Plant')
+   time       = models.DateTimeField('Date mesured')
+   value      = models.IntegerField()
+   sec        = models.IntegerField()
+   plant_type = models.ForeignKey('PlantType')
+
+   def is_OK(self):
+      if plant_type.mini_soil < value < plant_type.maxi_soil:
+         return True
+      else:
+         return False
+
+class TempHumMesure(models.Model):
+   time = models.DateTimeField('Date mesured')
+   temp = models.IntegerField()
+   humi = models.IntegerField()
 
 
-   
+
+   def is_too_hot(self):
+      if temp > 40:
+         return True
+      else:
+         return False
+
+   def is_to_cold(self):
+      if temp < 10:
+         return True
+      else:
+         return False
+
+
+   def is_OK(self):
+      if not self.is_too_hot() and not self.is_too_cold():
+         return True
+
+      else:
+         return False
+
+
+class PlantType(models.Model):
+    
+    name        = models.TextField(max_length=50)
+    mini_soil   = models.IntegerField()
+    maxi_soil   = models.IntegerField()
+    mini_temp   = models.IntegerField()
+    maxi_temp   = models.IntegerField()
+    light_growth= models.IntegerField()
+    light_bloom = models.IntegerField()
+    growth_time = models.IntegerField()
+    
+    def is_ok(self, ):
+        return True
+    
+class Plant(models.Model):
+    
+    espece   = models.ForeignKey(PlantType) 
+    position = models.IntegerField()
+    name     = models.
+
