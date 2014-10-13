a8d74cda09af009e7d340d6d4ed3ba2c11166e48
Benjamin Godard <ben@hacktopie.net>
2014-10-13 19:17:42 +0200

    some more shit, no clue what

---
+++ brain/gui_jardin/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from core.models import PlantType
+
+# Register models here
+
+admin.site.register(PlantType)
---
+++ brain/gui_jardin/datalogger/control.py
@@ -0,0 +1,12 @@
+from django.utils import timezone
+from datalogger.models import *
+from core.models import PlantType
+
+
+class checks_imba():
+    """
+    Regroups all checking methods on the health of the garden. All code checks are done in tests
+    """
+
+
+
