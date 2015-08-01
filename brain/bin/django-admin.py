9c4d42361ef0bb659803af0ab4eb870988187c68
Benjamin Godard <bgodard@aortasoftware.com>
2015-08-01 17:55:43 +0200

    removed virutal env shit from repo.

--- brain/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/usr/src/jardin_magique.git/brain/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.6.5','django-admin.py'
-__requires__ = 'Django==1.6.5'
-import pkg_resources
-pkg_resources.run_script('Django==1.6.5', 'django-admin.py')
