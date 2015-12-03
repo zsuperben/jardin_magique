1213f36cbfdfc05acdd5c43e9db5f2d5c428f92d
Benjamin Godard <bgodard@aortasoftware.com>
2015-12-03 20:59:39 +0100

    Added temperature parsing and added more readability to the code

--- brain/core_logger/rekpars.py
+++ brain/core_logger/rekpars.py
@@ -2,35 +2,48 @@ __author__ = 'zsb'
 import re
 import datetime
 
+
+def get_plant(request):
+    pm = re.match(r'.*plant=(\d*).*', request)
+    if pm:
+        return int(pm.groups()[0])
+
+
+def get_soil(request):
+    sm = re.match(r'.*soil=(\d*).*', request)
+    if sm:
+        return int(sm.groups()[0])
+
+def get_zone(request):
+    zm = re.match(r'.*zone=(\d*).*', request)
+    if zm:
+        return int(zm.groups()[0])
+
+def get_temperature(request):
+    tm = re.match(r'.*temp=(-{0,1}\d*\.\d*)', request)
+    if tm:
+        return float(tm.groups()[0])
+
+
+
 def parse_request(request):
 
     print(request)
     request = str(request)
     lines = request.split('\n')
 
-    def get_plant(request):
-        pm = re.match(r'.*plant=(\d*).*', request)
-        if pm:
-            return int(pm.groups()[0])
-            print "Got match pm"
-            print ret_dict
 
-    def get_soil(request):
-        sm = re.match(r'.*soil=(\d*).*', request)
-        if sm:
-            print "Got match sm"
-            return int(sm.groups()[0])
-            print(ret_dict)
-    def get_zone(request):
-        zm = re.match(r'.*zone=(\d*).*', request)
-        if zm:
-            return int(zm.groups()[0])
-            print(ret_dict)
     retlist = []
     for line in lines:
+        # Real parsing happens here.
         d = {}
-        d['zone'], d['plant'], d['soil'], d['time'] = get_zone(request), get_plant(request), get_soil(request), "'" + datetime.datetime.now().isoformat(sep=' ') +"'"
-        if d['zone'] and d['plant'] and d['soil'] and d['time']:
+        #This ugly line is too long and too obscure to read
+        d['zone'] = get_zone(request)
+        d['plant'] = get_plant(request)
+        d['soil'] = get_soil(request)
+        d['time'] = "'" + datetime.datetime.now().isoformat(sep=' ') + "'"
+        d['temp'] = 27
+        if d['zone'] and d['plant'] and d['soil'] and d['time'] and d['temp']:
             retlist.append(d)
 
     if len(retlist) > 0:
