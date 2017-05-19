a037b63ed776e8812662f065ad694ce1ee945cb8
Benjamin Godard <ben@hacktopie.net>
2017-05-19 19:51:41 +0000

    fixing meteo

--- brain/core_logger/meteo/__init__.py
+++ brain/core_logger/meteo/__init__.py
@@ -1,25 +1,29 @@
 __author__ = "zsb"
 
 from tornado_json.requesthandlers import APIError, APIHandler
-from tornado import schema
 import json
 import datetime
 from db import insert_dict_into_db, get_connection
 import requests
+import logging
+
+logger = logging.getLogger('api')
 
 APPID = "d70b47c0b3a4d2bc343df3973dee2ed0"
 
-def meteoHandler(APIHandler):
+class meteoHandler(APIHandler):
     def get(self):
         try:
             pluie = 0 
             m = requests.get("http://api.openweathermap.org/forecast?q=cahan,fr&appid=%s&units=metric&lang=fr" % APPID )
-            if m.response_code == 200:
+            if m.status_code == 200:
                 d = json.loads(m.content.decode())
+                logger.notice(d)
                 for prev in d['list']:
                     if 'rain' in prev.keys():
                         pluie = pluie + prev['rain']['3h']
 
             self.write({"pluie": pluie})
-        except:
+        except Exception as e:
+            logger.error(e)
             raise APIError(500)
